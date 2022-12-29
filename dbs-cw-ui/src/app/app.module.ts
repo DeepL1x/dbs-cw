ad0e1a4215e20834ac4382039d72218df8b962bf
DeepL1x <potato1869@gmail.com>
2022-12-29 17:15:56 +0200

    Added corse filter, added entity services for every entity on front-end part.

--- dbs-cw-ui/src/app/app.module.ts
+++ dbs-cw-ui/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ArticleService} from "./enities/article/article.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArticleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
