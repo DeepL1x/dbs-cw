d8522554acdd2ab4b5230ebb2ff6e579f051bb4e
DeepL1x <potato1869@gmail.com>
2022-12-31 15:28:08 +0200

    fixed connection to jedis

--- src/main/java/com/cavemen/dbscw/conf/Config.java
+++ src/main/java/com/cavemen/dbscw/conf/Config.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-@Configuration
-public class Config {
-
-  @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-//    JedisConnectionFactory jedisConFactory
-//        = new JedisConnectionFactory();
-//    jedisConFactory.setHostName("localhost");
-//    jedisConFactory.setPort(6379);
-    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
-    return new JedisConnectionFactory(redisStandaloneConfiguration);
-  }
-
-}
+//@Configuration
+//public class Config {
+//
+//  @Bean
+//  JedisConnectionFactory jedisConnectionFactory() {
+////    JedisConnectionFactory jedisConFactory
+////        = new JedisConnectionFactory();
+////    jedisConFactory.setHostName("localhost");
+////    jedisConFactory.setPort(6379);
+//    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+//    return new JedisConnectionFactory(redisStandaloneConfiguration);
+//  }
+//
+//}
